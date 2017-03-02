35be579faf09c51501bb2e0a6670c39413a8ce05
vermanil <anilrajverma1996@gmail.com>
2017-03-02 16:39:18 +0530

    done

--- routes/index.js
+++ routes/index.js
@@ -101,41 +101,42 @@ router.post('/do', function (req, res, next) {
 router.get('/notify', function (req, res, next) {
    console.log('have to Notify');
     var emailList = [];
-    user.find({email: { $exists: true }}, function (err, data){
+    user.find({email: { $exists: true }}, function (err, data) {
         if (err)
             console.log(err);
         l = data.length;
         emailList = [data[0].email];
-        for(i=1;i<l;i++)
-        {
-           emailList.push(data[i].email);
-        }
-        emailList.push('anilrajverma1996@gmail.com');
-        console.log(emailList[0]);
-    });
-    var tex = 'New task has added in to do list';
-    var smtpTransport = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: "noreplycheck87@gmail.com",
-            pass: "qwertyuiop!@"
-        }
-    });
+        for (i = 1; i < l; i++) {
+            emailList.push(data[i].email);
+            emailList.push('anilrajverma1996@gmail.com');
+            console.log(emailList[0]);
+            console.log(emailList[0]);
+            var tex = 'New task has added in to do list';
+            var smtpTransport = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: "noreplycheck87@gmail.com",
+                    pass: "qwertyuiop!@"
+                }
+            });
 
-    var mailOptions={
-        from : 'noreplycheck87@gmail.com',
-        to : 'rohitdharavath@gmail.com',
-        subject : 'task added',
-        text : tex
-    }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-        if(error){
-            console.log(error);
-            res.end("error");
-        }else{
-            console.log("Message sent: " + response.message);
-            res.end("sent");
+            var mailOptions = {
+                from: 'noreplycheck87@gmail.com',
+                to: emailList[i],
+                subject: 'task added',
+                text: tex
+            };
+            console.log("'" + emailList[2] + "'");
+            console.log(mailOptions);
+            smtpTransport.sendMail(mailOptions, function (error, response) {
+                if (error) {
+                    console.log(error);
+                    res.end("error");
+                } else {
+                    console.log("Message sent: " + response.message);
+                    res.end("sent");
+                }
+            });
         }
     });
 });
